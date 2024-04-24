6380e7be8bd873682ab6755cb30955470ae5c56d
Reggi76 <kots.genrix76@mail.ru>
2024-04-24 09:10:29 +0400

    Добавьте файлы проекта.

---
+++ SocialNetwork/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Models
+{
+    public class Message
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public int SenderId { get; set; }
+        [Required]
+        public int ReceverId { get; set; }
+    }
+}
---
+++ SocialNetwork/Models/UserSession.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Models
+{
+    public class UserSession
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+    }
+}
